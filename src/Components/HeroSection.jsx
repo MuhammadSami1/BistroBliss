e8d42680915783890f410a829073ffe918fce688
Muhammad Sami <muhammadsami1242@gmail.com>
2024-07-04 16:35:33 +0500

    bug fixes

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -23,14 +23,14 @@ const HeroSection = () => {
             textAlign: "left",
           }}
         >
-          <div className="sm:pt-28 pb-4 text-white leading-tight text:lg sm:text-2xl lg:text-7xl max-w-lg libre-baskerville-regular">
+          <div className="sm:pt-28 pb-2 sm:pb-4 text-white leading-tight text:lg sm:text-2xl lg:text-7xl max-w-lg libre-baskerville-regular">
             Best food for your taste
           </div>
-          <div className="text-white text-sm lg:text-lg sm:max-w-lg pt-4 tracking-wide font-semibold">
+          <div className="text-white text-xs sm:text-sm lg:text-lg sm:max-w-lg pt-2 sm:pt-4 tracking-wide font-semibold">
             <p>Discover delectable cuisine and unforgettable moments</p>
             <p className="sm:px-24">in our welcoming, culinary haven.</p>
           </div>
-          <div className="flex sm:px-20 pt-4 sm:pt-10 sm:gap-10 gap-2">
+          <div className="flex sm:px-20 pt-2 sm:pt-10 sm:gap-10 gap-2">
             <button className="text-white rounded-3xl px-1 py-1 sm:px-6 sm:py-4 text-sm bg-red-800">
               <Link to="/contact">Book A Table</Link>
             </button>
