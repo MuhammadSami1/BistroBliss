df4a097617a0d8ab2180a9cc62861146b3dd3eb3
Muhammad Sami <muhammadsami1242@gmail.com>
2024-07-08 22:52:02 +0500

    project compelete

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -266,3 +266,38 @@ const Food = [
 ];
 
 export default Food;
+
+export const Interior = [
+  {
+    id: "1",
+    img: "./Images/interior/interior1.webp",
+  },
+  {
+    id: "2",
+    img: "https://images.unsplash.com/photo-1551632436-cbf8dd35adfa?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "3",
+    img: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "4",
+    img: "https://images.unsplash.com/photo-1549488344-1f9b8d2bd1f3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: "5",
+    img: "./Images/interior/interior5.webp",
+  },
+  {
+    id: "6",
+    img: "./Images/interior/interior6.webp",
+  },
+  {
+    id: "7",
+    img: "./Images/interior/interior7.webp",
+  },
+  {
+    id: "8",
+    img: "https://images.unsplash.com/photo-1525610553991-2bede1a236e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
